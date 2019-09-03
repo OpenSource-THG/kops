4e31e6de86061b6d5377c4c863b8605596a10347
Teodor Todorov <teodor.todorov@thehutgroup.com>
2019-09-03 15:08:21 +0100

    Add annotations to support OS booting from volume

--- pkg/model/openstackmodel/servergroup.go
+++ pkg/model/openstackmodel/servergroup.go
@@ -64,6 +64,14 @@ func (b *ServerGroupModelBuilder) buildInstances(c *fi.ModelBuilderContext, sg *
 	igMeta[openstack.INSTANCE_GROUP_GENERATION] = fmt.Sprintf("%d", ig.GetGeneration())
 	igMeta[openstack.CLUSTER_GENERATION] = fmt.Sprintf("%d", b.Cluster.GetGeneration())
 
+	if e, ok := ig.ObjectMeta.Annotations["openstack.kops.io/osVolumeBoot"]; ok {
+		igMeta["openstack.kops.io/osVolumeBoot"] = e
+	}
+
+	if v, ok := ig.ObjectMeta.Annotations["openstack.kops.io/osVolumeSize"]; ok {
+		igMeta["openstack.kops.io/osVolumeSize"] = v
+	}
+
 	startupScript, err := b.BootstrapScript.ResourceNodeUp(ig, b.Cluster)
 	if err != nil {
 		return fmt.Errorf("Could not create startup script for instance group %s: %v", ig.Name, err)
